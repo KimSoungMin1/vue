0dabb03cd27c87ec4921dc5773be60ab699c7ee3
KimSeongMin <tjdals247470@naver.com>
2023-01-20 14:50:57 +0900

    vue평가

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>   
+export default{
+  name:'App',
+  data() {
+    return {
+      count: '0'
+    }}
+  }
+  </script>
+<template>
+  <h3>{{count}}</h3>
+  <p>vue로 확인하는 웹 페이지 입니다 아래 버튼을<br> 누르시면 값이 1씩 증가합니다</p>
+  <button  @click='count++'>1+</button>
+</template>
